3f95aa86d07bf4c8a51ba829c2a191b1bde690d1
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2022-03-23 10:16:15 -0300

    Ultimas alterações

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
 const selectElement = (element) => document.querySelector(element);
 
 const menuList = [
-  { idMenu: 1, descricao: 'Usuários', url: 'usuario', nivel: 1, menuPai: null, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 2, descricao: 'Perfis', url: 'perfil', nivel: 1, menuPai: null, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 3, descricao: 'Menus', url: 'menu', nivel: 1, menuPai: null, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 4, descricao: 'Listagem de Usuários', url: 'usuario-list', nivel: 2, menuPai: 1, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 5, descricao: 'Formulário de Usuários', url: 'usuario-form', nivel: 2, menuPai: 1, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 6, descricao: 'Listagem de Perfis', url: 'perfil-list', nivel: 2, menuPai: 2, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 7, descricao: 'Formulário de Perfis', url: 'perfil-form', nivel: 2, menuPai: 2, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 8, descricao: 'Listagem de Menus', url: 'menu-list', nivel: 2, menuPai: 3, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 9, descricao: 'Formulário de Menus', url: 'menu-form', nivel: 2, menuPai: 3, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
-  { idMenu: 10, descricao: 'Terceiro Nível', url: '', nivel: 3, menuPai: 9, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 01, descricao: '1. Primeiro Nível', url: '', nivel: 1, menuPai: null, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 02, descricao: '2. Primeiro Nível', url: '', nivel: 1, menuPai: null, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 03, descricao: '3. Primeiro Nível', url: '', nivel: 1, menuPai: null, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 04, descricao: '1.1. Segundo Nível', url: '', nivel: 2, menuPai: 1, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 05, descricao: '1.2. Segundo Nível', url: 'usuario-form', nivel: 2, menuPai: 1, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 06, descricao: '2.1. Segundo Nível', url: 'perfil-list', nivel: 2, menuPai: 2, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 07, descricao: '2.2. Segundo Nível', url: 'perfil-form', nivel: 2, menuPai: 2, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 08, descricao: '3.1. Segundo Nível', url: 'menu-list', nivel: 2, menuPai: 3, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 09, descricao: '3.2. Segundo Nível', url: '', nivel: 2, menuPai: 3, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 10, descricao: '3.2.1. Terceiro Nível', url: 'level3', nivel: 3, menuPai: 9, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
+  { idMenu: 11, descricao: '1.1.1. Terceiro Nível', url: 'level3', nivel: 3, menuPai: 4, dhCriacao: '22/03/2022', matriculaCriacao: 'Z236415' },
 ];
 
 const menu = selectElement('ul.nav-list');
@@ -21,9 +22,10 @@ console.log(firstLevel);
 function buildTree(item) {
   const li = document.createElement('li');
   const link = document.createElement('a');
-
   link.innerHTML = item.descricao;
-  link.href = item.url;
+  if (item.url !== '') {
+    link.href = item.url;
+  }
   li.append(link);
 
   const children = menuList.filter(child => child.menuPai === item.idMenu);
